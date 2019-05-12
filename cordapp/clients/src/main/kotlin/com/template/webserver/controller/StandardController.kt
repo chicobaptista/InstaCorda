8c303d66c651e2b30d563df81eed53811ec79d12
tcardozo <tcardozo@bluchain.tech>
2019-05-11 22:39:55 -0300

    SpringBoot setup

--- cordapp/clients/src/main/kotlin/com/template/webserver/controller/StandardController.kt
+++ cordapp/clients/src/main/kotlin/com/template/webserver/controller/StandardController.kt
@@ -3,39 +3,45 @@ package com.template.webserver.controller
 
 import com.template.states.ExampleState
 import com.template.webserver.NodeRPCConnection
-import net.corda.core.contracts.ContractState
 import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.vault.QueryCriteria
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.time.ZoneId
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 /**
  * A CorDapp-agnostic controller that exposes standard endpoints.
  */
 @RestController
 @RequestMapping("/api") // The paths for GET and POST requests are relative to this base path.
-class StandardController(
-        private val rpc: NodeRPCConnection) {
-
+class StandardController(private val rpc: NodeRPCConnection) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(RestController::class.java)
     }
 
-    init{
-        // Define a Vault observable for the Example State
-        val exampleStateVaultObservable = rpc.proxy.vaultTrack(ExampleState::class.java).updates
-        exampleStateVaultObservable.subscribe { update ->
-            update.produced.forEach { (state) ->
-                logger.info("Vault update :"+state.data)
-            }
-        }
-    }
+//    init{
+//        // Define a Vault observable for the Example State
+//        val exampleStateVaultObservable = rpc.proxy.vaultTrack(ExampleState::class.java).updates
+//        exampleStateVaultObservable.subscribe { update ->
+//            update.produced.forEach { (state) ->
+//                logger.info("Vault update :"+state.data)
+//            }
+//        }
+//    }
+
+    private val myLegalName = rpc.proxy.nodeInfo().legalIdentities.first().name
 
     private val proxy = rpc.proxy
+
+    @GetMapping(value = "me", produces = arrayOf(MediaType.APPLICATION_JSON))
+    fun whoami() = mapOf("me" to myLegalName)
 
     @GetMapping(value = "/status", produces = arrayOf("text/plain"))
     private fun status() = "200"
@@ -61,6 +67,17 @@ class StandardController(
     @GetMapping(value = "/flows", produces = arrayOf("text/plain"))
     private fun flows() = proxy.registeredFlows().toString()
 
-    @GetMapping(value = "/states", produces = arrayOf("text/plain"))
-    private fun states() = proxy.vaultQueryBy<ContractState>().states.toString()
+    @GetMapping(value = "/states", produces = arrayOf(MediaType.APPLICATION_JSON))
+    private fun states(): Response {
+        val criteria = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.ALL)
+
+        val result = proxy.vaultQueryBy<ExampleState>(criteria = criteria).states
+
+        return Response.status(Response.Status.CREATED).entity( result ).build()
+    }
+
+
+
+
+
 }
